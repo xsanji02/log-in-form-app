b4a43a9756a5017b937911285e840adeabd4829f
Josuel Dela Cruz M <114049497+xsanji02@users.noreply.github.com>
2023-05-30 03:51:22 +0800

    new design

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,33 +1,55 @@
-import React from "react";
-import './responsive/Mobile.css'
+import React, { useState } from "react";
+import "./responsive/Mobile.css";
+import { AiFillEyeInvisible } from "react-icons/ai";
+import { AiFillEye } from "react-icons/ai";
 
 export const Login = ({ inputChange, handleSubmit }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const showPass = (e) => {
+    setShowPassword((prevState) => !prevState);
+  };
+
   return (
     <div className="form-parent">
-        <h2>Log-in Form</h2>
+      <h2>Log-in Form</h2>
       <div className="form-child">
         <form onSubmit={handleSubmit}>
-          <input
+          <div className="email-div">
+          <input 
             type="email"
             placeholder="example@email.com"
             name="email"
             onChange={inputChange}
             required
           />
-          <input
-            type="password"
-            placeholder="Password"
-            name="pass"
-            onChange={inputChange}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            name="confirmPass"
-            onChange={inputChange}
-            required
-          />
+          </div>
+          <div className="revealPass">
+            <input
+              id="showPass"
+              type="password"
+              placeholder="Password"
+              name="pass"
+              onChange={inputChange}
+              required
+            />
+            <label htmlFor="showPass" onClick={showPass}>
+              {showPassword !== true ? <AiFillEyeInvisible /> : <AiFillEye />}
+            </label>
+          </div>
+          <div className="revealPass">
+            <input
+              id="showPass"
+              type="password"
+              placeholder="Confirm Password"
+              name="confirmPass"
+              onChange={inputChange}
+              required
+            />
+            <label htmlFor="showPass" onClick={showPass}>
+              {showPassword !== true ? <AiFillEyeInvisible /> : <AiFillEye />}
+            </label>
+          </div>
           <button>Log in</button>
         </form>
       </div>
